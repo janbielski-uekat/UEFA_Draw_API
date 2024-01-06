c359987070017081f7141bd4874910abdd300717
Jan Bielski <jan.bielski@edu.uekat.pl>
2024-01-06 21:21:35 +0100

    Creating basic serializers

--- schema/schemas.py
+++ schema/schemas.py
@@ -1,1 +1,11 @@
+def individual_serial(team) -> dict:
+    return{
+        "id": str(team["_id"]),
+        "name": team["name"],
+        "nationality": team["nationality"],
+        "place_in_group": team["place_in_group"],
+        "group": team["group"]
+    }
 
+def list_serial(teams) -> list:
+    return[individual_serial(team) for team in teams]
