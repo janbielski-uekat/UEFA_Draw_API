087182b72182ce5da895dc7f8a90ad4a7f7efcfd
Jan Bielski <jan.bielski@edu.uekat.pl>
2024-01-06 21:51:13 +0100

    Add basic routes

--- routes/route.py
+++ routes/route.py
@@ -10,3 +10,15 @@ router = APIRouter()
 async def get_teams():
     teams = list_serial(collection_name.find())
     return teams
+
+@router.post("/")
+async def post_team(team: Team):
+    collection_name.insert_one(dict(team))
+
+@router.put("/{id}")
+async def put_team(id: str, team: Team):
+    collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(team)})
+
+@router.delete("/{id}")
+async def delete_team(id: str):
+    collection_name.find_one_and_delete({"_id": ObjectId(id)})
